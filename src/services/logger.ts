e8c9fdfa2b3dadc9cba4bc7b74494fa77055d065
Hamzat Abdul-muizz <103611443+canhamzacode@users.noreply.github.com>
2024-08-26 21:53:38 +0200

    feat: logger setup (#2)

---
+++ src/services/logger.ts
@@ -0,0 +1,17 @@
+import winston from "winston";
+
+const { combine, timestamp, printf } = winston.format
+
+const logger = winston.createLogger({
+    level: "info",
+    format: combine(
+        timestamp(),
+        printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
+    ),
+    transports: [
+        new winston.transports.File({ filename: "logs/error.log", level: "error" }),
+        new winston.transports.File({ filename: "logs/combined.log" })
+    ]
+})
+
+export default logger;
