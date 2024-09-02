6525b489cf7f8b538ec78ebf796dcc81e87f810c
Hamzat Abdul-muizz <103611443+canhamzacode@users.noreply.github.com>
2024-09-02 20:59:25 +0100

    feat: Configure CI pipline (#3)
    * feat: ci pipeline initial commit
    
    * fix: update @typescript-eslint/eslint-plugin to v7
    
    * fix: update ci configuration
    
    * ci: fixing ci
    
    * ci: change npm to yarn
    
    * fix: eslint
    
    ---------
    
    Co-authored-by: John Oshalusi <john.oshalusi@iohk.io>

---
+++ eslint.config.mjs
@@ -0,0 +1,40 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default [
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    ignores: ['dist', 'node_modules', 'data']
+  },
+  {
+    files: ['src/**/*.{ts,js,tsx,jsx}', 'test/**/*.{ts,js,tsx,jsx}'],
+    rules: {
+      'unicorn/filename-case': 'off',
+      'import/prefer-default-export': 'off',
+      'unicorn/no-null': 'off',
+      'unicorn/prevent-abbreviations': 'off',
+      'no-console': 'error',
+      'no-restricted-syntax': 'off',
+      'no-duplicate-imports': 'error',
+      'import/no-extraneous-dependencies': 'off',
+      '@typescript-eslint/lines-between-class-members': 'off',
+      '@typescript-eslint/no-namespace': 'off',
+      'no-underscore-dangle': 'off',
+      'spaced-comment': 'off',
+      'import/extensions': 'off',
+      'comma-dangle': 'off',
+      '@typescript-eslint/comma-dangle': 'off',
+      'unicorn/no-anonymous-default-export': 'off',
+      'unicorn/prefer-string-replace-all': 'off'
+    }
+  },
+  {
+    languageOptions: {
+      globals: globals.node
+    }
+  }
+];
--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,10 @@ import { logger } from './services';
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
+app.get('/', (_, res) => {
   res.send('Hello, World!');
 });
 
 app.listen(port, () => {
   logger.info(`Server listening on port ${port}`);
-}); 
+});
